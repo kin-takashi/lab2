dbbc6e6038c5f458e6aa25f171f918b95085fb76
Nam <ngtnam111@gmail.com>
2023-10-25 11:40:45 +0700

    'a'

--- Source.cpp
+++ Source.cpp
@@ -4,10 +4,21 @@ using namespace std;
 int tong(int a, int b);
 int hieu(int a, int b);
 int tich(int a, int b);
-float thuong(int a, int b);
+float thuong(int a, int b)
+{
+	if(b == 0)
+	{
+		cout <<"KO CHIA CHO 0";
+		
+	}
+	
+	return a/b;
+	
+};
 int main()
 {
 	cout << "DAY LA CHUONG TRINH DE THUC HANH GIT";
+	thuong(a,b);
 	system("pause");
 	return 0;
 }
