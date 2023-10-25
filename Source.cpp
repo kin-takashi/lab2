0264c21d5597bb6cce3f390dd5d68cf591e8d501
HuyPhan <phantanhuy1234@gmail.com>
2023-10-25 11:55:14 +0700

    PhanTanHuy

--- Source.cpp
+++ Source.cpp
@@ -3,11 +3,17 @@
 using namespace std;
 int tong(int a, int b);
 int hieu(int a, int b);
-int tich(int a, int b);
+int tich(int a, int b){
+	return a*b;
+}
 float thuong(int a, int b);
 int main()
 {
+	
 	cout << "DAY LA CHUONG TRINH DE THUC HANH GIT";
+	tich(2*3);
+	
 	system("pause");
+
 	return 0;
 }
